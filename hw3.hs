2b4b452ef1307d3eea3313cc9d80afd70c61ffe7
t.vladimir <vovatatosyan2000@mail.ru>
2020-11-18 00:42:08 +0300

    Added reverse each half

--- hw3.hs
+++ hw3.hs
@@ -1,22 +1,17 @@
--- razbit' massiv popolam i perevernut' polovinki
-{-middleSplitThenSort :: (Ord a) => [a] -> [a]
-middleSplitThenSort [] = []
-middleSplitThenSort [x] = [x]
-middleSplitThenSort [x,y] = [y, x]
-middleSplitThenSort (x:xs) =
-    let
-        (f, g) = del f g
-    in
-        (xs, [])
-    where 
-        del f g 
-            | length f == length g = (f, g)
-            | length f > length g = ((init f), (last f) ++ g)-}
+{-
+    Split the list in half and flip each half.
+-}
+middleSplitThenFlip :: [a] -> [a]
+middleSplitThenFlip [] = []
+middleSplitThenFlip xs = list
+    where
+        list = (rev beg) ++ mid ++ (rev end)
 
-halve :: [a] -> ([a], [a], [a])
-halve [] = ([],[],[])
-halve xs = go xs xs
-  where
-    go (x:xs) (_:_:ys) = let (first, mid, last) = go xs ys in (x:first, mid,  last)
-    go (x:xs) [_] = ([], [x], xs)
-    go (x:xs) []  = ([], [], x:xs)
+        (beg, mid, end) = go xs xs
+        go (x:xs) (_:_:ys) = let (beg, mid, end) = go xs ys in (x:beg, mid, end)
+        go (x:xs) [_] = ([], [x], xs)
+        go (x:xs) []  = ([], [], x:xs)
+
+        rev [] = []
+        rev [x] = [x]
+        rev (x:xs) = rev xs ++ [x]
