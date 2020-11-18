cb3c5591334edcb2ba06a12a23da5619322a6739
t.vladimir <vovatatosyan2000@mail.ru>
2020-11-18 14:08:11 +0300

    Added a separate function for dividing the list into three parts

--- hw3.hs
+++ hw3.hs
@@ -3,15 +3,32 @@
 -}
 middleSplitThenFlip :: [a] -> [a]
 middleSplitThenFlip [] = []
-middleSplitThenFlip xs = list
+middleSplitThenFlip zs = list
     where
         list = (rev beg) ++ mid ++ (rev end)
 
-        (beg, mid, end) = go xs xs
-        go (x:xs) (_:_:ys) = let (beg, mid, end) = go xs ys in (x:beg, mid, end)
+        (beg, mid, end) = go zs zs
+        go (x:xs) (_:_:ys) = 
+            let 
+                (beg, mid, end) = go xs ys 
+            in 
+                (x:beg, mid, end)
         go (x:xs) [_] = ([], [x], xs)
         go (x:xs) []  = ([], [], x:xs)
 
         rev [] = []
         rev [x] = [x]
         rev (x:xs) = rev xs ++ [x]
+
+
+halve :: [a] -> ([a], [a], [a])
+halve [] = ([], [], [])
+halve xs = go xs xs
+    where
+        go (x:xs) (_:_:ys) =
+            let
+                (beg, mid, end) = go xs ys
+            in
+                (x:beg, mid, end)
+        go (x:xs) [_] = ([], [x], xs)
+        go xs [] = ([], [], xs) 
